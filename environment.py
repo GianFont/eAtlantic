9b5e3d6e01bb56eff602a1890f124dc01605dd9c
GianFont <39124963+GianFont@users.noreply.github.com>
2022-04-29 19:15:48 +0200

    add envronment

--- environment.py
+++ environment.py
@@ -8,24 +8,24 @@ class Env:
     def __init__(self, width=7, height=6):
         self.width = width
         self.height = height
-        self.board = np.full((self.width, self.height), 0)
+        self.network = np.full((self.width, self.height), 0)
 
     def step(self, col_idx):
-
-        step_player = self.get_current_player()
+    ### function to move from one state to the next
+    # state is the current state s_0 of the network - the positions of the colored discs in the suspended grid
 
         # invalid column index is provided
         if col_idx >= self.width:
-            state = self.board.copy()
+            state = self.network.copy()
             # reward = -100
             reward = -1
             result = -1
             return state, reward, result
 
         # the provided column is full
-        row_idx = np.argmin(self.board, 1)[col_idx]
-        if self.board[col_idx, row_idx] != 0:
-            state = self.board.copy()
+        row_idx = np.argmin(self.network, 1)[col_idx]
+        if self.network[col_idx, row_idx] != 0:
+            state = self.network.copy()
             # reward = -100
             reward = -1
             result = -1
@@ -33,9 +33,9 @@ class Env:
 
         # the provided column is valid
         # drop the chip to this column
-        self.board[col_idx, row_idx] = step_player
+        self.network[col_idx, row_idx] = step_player
 
-        state = self.board.copy()
+        state = self.network.copy()
         result = self._judge(col_idx, row_idx)
         if result == step_player:
             reward = 1
@@ -48,22 +48,22 @@ class Env:
 
     def simulate(self, test_state, col_idx):
         # backup the current state and current player
-        snapshot = self.board.copy()
+        snapshot = self.network.copy()
         # play on the given state
-        self.board = test_state.copy()
+        self.network = test_state.copy()
         state, reward, result = self.step(col_idx)
         # restore the state and current player
-        self.board = snapshot
+        self.network = snapshot
 
         return state, reward, result
 
     def _judge(self, col_idx, row_idx):
         result = 0
-        player = self.board[col_idx, row_idx]
+        player = self.network[col_idx, row_idx]
 
         # vertical
         if row_idx >= 3:
-            check_range = self.board[col_idx, np.arange(row_idx - 3, row_idx + 1)]
+            check_range = self.network[col_idx, np.arange(row_idx - 3, row_idx + 1)]
             if len(check_range[check_range != player]) == 0:
                 result = player
                 # print('Player', player, 'won vertically!')
@@ -73,14 +73,14 @@ class Env:
             if 0 <= idx < self.width:
                 new_range = np.arange(max(idx, idx - 3), min(self.width, idx + 4))
                 if len(new_range) >= 4:
-                    check_range = self.board[new_range, row_idx]
+                    check_range = self.network[new_range, row_idx]
                     if len(check_range[check_range != player]) == 0:
                         result = player
                         # print('Player', player, 'won horizontally!')
                         break
         # diagonal
-        diag_bwd = np.diagonal(np.rot90(self.board), offset=((row_idx + 1) - (self.width - (col_idx + 1))))
-        diag_fwd = np.diagonal(np.flipud(np.rot90(self.board)), offset=col_idx - row_idx)
+        diag_bwd = np.diagonal(np.rot90(self.network), offset=((row_idx + 1) - (self.width - (col_idx + 1))))
+        diag_fwd = np.diagonal(np.flipud(np.rot90(self.network)), offset=col_idx - row_idx)
         # print(diag_bwd)
         # print(diag_fwd)
         if len(diag_bwd) >= 4:
@@ -100,7 +100,7 @@ class Env:
                     # print('Player', player, 'won forward diagonally!')
                     break
         # draw
-        if len(self.board.reshape(self.width * self.height).nonzero()[0]) == self.width * self.height:
+        if len(self.network.reshape(self.width * self.height).nonzero()[0]) == self.width * self.height:
             # print('Draw game!')
             result = 3
 
@@ -111,7 +111,7 @@ class Env:
 
     def get_valid_actions(self, state=None):
         if state is None:
-            state = self.board.copy()
+            state = self.network.copy()
         actions = []
         for col_idx in range(self.width):
             if np.min(state, 1)[col_idx] > 0:
@@ -121,13 +121,13 @@ class Env:
         return actions
 
     def reset(self):
-        self.board = np.full((self.width, self.height), 0)
+        self.network = np.full((self.width, self.height), 0)
 
-    def to_str(self, board=None):
+    def to_str(self, network=None):
         string = os.linesep
-        if board is None:
-            board = self.board
-        b = np.rot90(board).reshape(self.width * self.height)
+        if network is None:
+            network = self.network
+        b = np.rot90(network).reshape(self.width * self.height)
         for idx, c in enumerate(b):
             c = int(c)
             if (idx + 1) % self.width > 0:
@@ -136,27 +136,27 @@ class Env:
                 string = '{}{}{}'.format(string, c, os.linesep)
         return string
 
-    def print(self, board=None):
-        print(self.to_str(board))
+    def print(self, network=None):
+        print(self.to_str(network))
 
     def get_state(self):
-        return self.board.copy().astype(dtype=np.float32)
+        return self.network.copy().astype(dtype=np.float32)
 
-    def get_mirror_state(self, board=None):
-        if board is None:
-            board = self.board
-        mirror = np.array(board)
+    def get_mirror_state(self, network=None):
+        if network is None:
+            network = self.network
+        mirror = np.array(network)
         for col_idx in range(self.width):
             for row_idx in range(self.height):
-                mirror[self.width - col_idx - 1, row_idx] = board[col_idx, row_idx]
+                mirror[self.width - col_idx - 1, row_idx] = network[col_idx, row_idx]
         # return mirror.astype(dtype=np.float32)
         return mirror
 
-    def get_inv_state(self, board=None):
-        if board is None:
-            board = self.board
-        # inv = board.copy().astype(dtype=np.float32)
-        inv = board.copy()
+    def get_inv_state(self, network=None):
+        if network is None:
+            network = self.network
+        # inv = network.copy().astype(dtype=np.float32)
+        inv = network.copy()
         for col_idx in range(self.width):
             for row_idx in range(self.height):
                 if inv[col_idx, row_idx] == 1:
@@ -165,20 +165,6 @@ class Env:
                     inv[col_idx, row_idx] = 1
         return inv
 
-    def get_current_player(self, state=None):
-        if state is None:
-            state = self.board.copy()
-        unique, counts = np.unique(state, return_counts=True)
-        player1_count = counts[np.where(unique == 1)]
-        player2_count = counts[np.where(unique == 2)]
-        if len(player1_count) == 0:
-            player1_count = 0
-        if len(player2_count) == 0:
-            player2_count = 0
-        if player1_count > player2_count:
-            return 2
-        else:
-            return 1
 
 
 def main():
